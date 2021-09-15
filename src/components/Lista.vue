20182b88bb463cabb1079908c9dbe2829926d038
Alexs Ruiz <xiopito2@gmail.com>
2021-09-15 16:54:54 -0600

    Vuex Implementation

---
+++ src/components/Lista.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <ul class="list-group">
+      <li v-for="(item, index) of arrayOrdenado" :key={index}
+      @click="aumentar(index)"
+        class="list-group-item d-flex justify-content-between align-items-center"
+      >
+        {{item.nombre}}
+        <span class="badge bg-primary rounded-pill">{{item.cantidad}}</span>
+      </li>
+    </ul>
+    
+  </div>
+<div class="text-center pt-4">
+    <button @click="restart" class="btn btn-outline-success">Restart</button>
+</div>
+</template>
+
+<script>
+import {mapState, mapMutations} from 'vuex'
+export default {
+  name: "Lista",
+  computed: {
+      ...mapState(['frutas']),
+      arrayOrdenado(){
+          return this.frutas.slice().sort( (a, b) => b.cantidad - a.cantidad)
+      }
+  },
+
+  methods: {
+      ...mapMutations(['aumentar', 'restart'])
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,32 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
 export default createStore({
   state: {
+    frutas: [
+      {
+        nombre: "Manzana",
+        cantidad: 0,
+      },
+      {
+        nombre: "Naranja",
+        cantidad: 0,
+      },
+      {
+        nombre: "Pera",
+        cantidad: 0,
+      },
+    ],
   },
   mutations: {
+    aumentar(state, index){
+      state.frutas[index].cantidad ++
+    },
+    restart(state){
+      state.frutas.forEach(el => {
+        el.cantidad = 0
+      })
+    }
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+  actions: {},
+  modules: {},
+});
